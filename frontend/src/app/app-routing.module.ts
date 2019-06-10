07b9e02971fffa366c7721a767e615336e694b94
Mohammed Aarif <ali.aliaarif@gmail.com>
2019-06-10 09:46:04 +0530

    10 June 2019

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Home1Component } from './home1/home1.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+
+
+localStorage.setItem('user', 'Admin');
+
+if(localStorage.has('user')){
+ 
+
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'register',
+    component: RegisterComponent
+  },
+
+];
+
+}else{
+const routes: Routes = [
+  {
+    path:'',
+    component: Home1Component
+  },
+
+];
+
+}
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { Home1Component } from './home1/home1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    Home1Component
   ],
   imports: [
     BrowserModule,
